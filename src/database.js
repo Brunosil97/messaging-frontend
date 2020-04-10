44e89b4f9e28571934b3a69c5c5e017136babf54
Amy Franz <amy.franz@gmail.com>
2020-04-10 18:22:10 +0100

    got data.js working

--- src/database.js
+++ src/database.js
@@ -1,52 +1,50 @@
+const users = [
+  {
+    email: "test1@gmail.com",
+    name: "bruno",
+    password: "brunotest",
+  },
+  {
+    email: "test2@gmail.com",
+    name: "amy",
+    password: "amytest",
+  },
+  {
+    email: "test3@gmail.com",
+    name: "simon",
+    password: "simontest",
+  },
+];
 
-// const users = [
-//     {
-//         email: "test1@gmail.com", 
-//         name: "bruno", 
-//         password: "brunotest"
-//     },
-//     {
-//         email: "test2@gmail.com",
-//         name: "amy",
-//         password: "amytest"
+const chats = [
+  {
+    chatname: "test1@gmail.com : test2@gmail.com",
+    messages: [
+      {
+        content: "this is a message",
+        sender: "test1@gmail.com",
+      },
+      {
+        content: "this is a second message",
+        sender: "test2@gmail.com",
+      },
+    ],
+    hasRead: false,
+  },
+  {
+    chatname: "test1@gmail.com : test3@gmail.com",
+    messages: [
+      {
+        content: "blah blah",
+        sender: "test1@gmail.com",
+      },
+      {
+        content: "blah",
+        sender: "test3@gmail.com",
+      },
+    ],
+    hasRead: false,
+  },
+];
 
-//     },
-//     {
-//         email: "test3@gmail.com",
-//         name: "simon",
-//         password: "simontest"
-//     }
-// ]
-
-// const chats = [
-//     {
-//         chatname: "test1@gmail.com : test2@gmail.com",
-//         messages: [
-//             {
-//                 content: "this is a message",
-//                 sender: "test1@gmail.com"
-//             },
-//             {
-//                 content: "this is a second message",
-//                 sender: "test2@gmail.com"
-//             }
-//         ],
-//         hasRead: false
-//     },
-//     {
-//         chatname: "test1@gmail.com : test3@gmail.com",
-//         messages: [
-//             {
-//                 content: "blah blah",
-//                 sender: "test1@gmail.com"
-//             },
-//             {
-//                 content: "blah",
-//                 sender: "test3@gmail.com"
-//             }
-//         ],
-//         hasRead: false
-//     }
-// ]
-
-// export default (users)(chats)
+export default { users, chats };
