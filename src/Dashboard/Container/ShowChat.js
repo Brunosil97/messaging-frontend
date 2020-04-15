b39d210ccb81e124500d4623b4c5f2e1138e4c83
Amy Franz <amy.franz@gmail.com>
2020-04-15 16:25:54 +0100

    creating message

--- src/Dashboard/Container/ShowChat.js
+++ src/Dashboard/Container/ShowChat.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ShowMessage from "../Components/ShowMessage";
 import TypeMessageBox from "../Components/TypeMessageBox";
+import API from "../../API";
 
 export default class ShowChat extends Component {
   constructor() {
@@ -11,7 +12,6 @@ export default class ShowChat extends Component {
   }
   render() {
     const chats = this.props.chat.messages.map((message, index) => {
-      debugger;
       return (
         <ShowMessage message={message} key={index} user={this.props.user} />
       );
@@ -31,10 +31,12 @@ export default class ShowChat extends Component {
     this.setState({ chatText: e.target.value });
   };
   onSubmitNewMessage = (e) => {
-    console.log({
-      message: this.state.chatText,
-      sender: "test1@gmail.com",
+    API.post("messages", {
+      content: this.state.chatText,
+      user_id: this.props.user.id,
+      chat_id: this.props.chat.id,
     });
+
     this.setState({ chatText: "" });
   };
 }
